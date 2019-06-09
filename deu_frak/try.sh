e5aa40fef8320d287a6cc108226e05af33ada033
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2019-06-09 23:07:51 +0200

    fix sample testing script

--- deu_frak/try.sh
+++ deu_frak/try.sh
@@ -6,8 +6,10 @@ lang=deu_frak$n
 bash buildscript.sh
 mv deu_frak.traineddata $lang.traineddata
 sudo cp $lang.traineddata /usr/share/tesseract-ocr/tessdata/
+cd samples
 for f in *.pbm
 do
-  tesseract $f $lang$f -l $lang
+  tesseract $f $lang$f -l $lang+deu_frak
+  tesseract $f deu_frak$f -l deu_frak
   cat $lang$f.txt
 done
